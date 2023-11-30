f4023129b7c6b1dd3fe9e8f03b526592e1c83a59
ridvanms <ridvans@abv.bg>
2023-11-30 20:21:49 +0200

    Update the home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 import { ApiServiceService } from '../Services/api-service.service';
 
 @Component({
@@ -6,7 +6,10 @@ import { ApiServiceService } from '../Services/api-service.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit {
+   @ViewChild('plant', { static: true }) plant!: ElementRef;
+  @ViewChild('plant1', { static: true }) plant1!: ElementRef;
+  @ViewChild('title', { static: true }) title!: ElementRef;
   species:any;
 
   constructor(
@@ -20,6 +23,20 @@ export class HomeComponent implements OnInit {
       error: (error) => {
         console.log(`Error: Could not load the data!`)
       }
+    })
+  }
+  ngAfterViewInit(): void {
+    const plant = this.plant?.nativeElement
+    const plant1 = this.plant1?.nativeElement
+    const title = this.title?.nativeElement
+
+    window.addEventListener('scroll', () => {
+      const value = window.scrollY;
+      
+      title.style.marginTop = value  + "px";
+      plant.style.marginLeft = value  + "px";
+      plant1.style.marginLeft = -(value ) + "px";
+      
     })
   }
   

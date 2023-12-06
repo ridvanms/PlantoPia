05766292cba03ebb73cfad7859fe5deb00f7f949
Ridvan <ridvans@abv.bg>
2023-12-06 11:31:24 +0200

    udpating

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
-export class AboutComponent {
-
+export class AboutComponent implements OnInit,AfterViewInit,OnDestroy {
+  constructor() {
+    // 
+  }
+  ngOnInit(): void {
+  }
+  ngAfterViewInit(): void {
+    window.scrollTo(0, 0);
+    // 
+  }
+  ngOnDestroy(): void {
+      // 
+  }
 }
